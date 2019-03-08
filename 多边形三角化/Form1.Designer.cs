2529e52218cd4ea32193c6df6b3467b42a9df018
YuYang Bai <307486164@qq.com>
2019-03-08 14:08:51 +0800

    the first version

---
+++ 多边形三角化/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace 多边形三角化 {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(718, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 426);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(699, 426);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClear);
+            this.Name = "Form1";
+            this.Text = "多边形三角化";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 多边形三角化/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace 多边形三角化 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        Brush[] brushes = new Brush[] {
+            Brushes.Yellow, Brushes.Violet, Brushes.SlateGray, Brushes.SpringGreen, Brushes.Tan, Brushes.Pink, Brushes.Teal, Brushes.Purple,
+            Brushes.OldLace, Brushes.Salmon, Brushes.OliveDrab, Brushes.DeepSkyBlue, Brushes.BurlyWood, Brushes.Thistle, Brushes.Turquoise, Brushes.Orchid,
+        };
+
+        List<Vec> polygon = new List<Vec>();
+
+        private void btnClear_Click(object sender, EventArgs e) {
+            polygon.Clear();
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) {
+            polygon.Add(new Vec(e.X, e.Y));
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e) {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            // 内部三角形
+            if (polygon.Count >= 3) {
+                List<int> tris = PolygonHelper.Resolve(polygon);
+                if (tris != null) {
+                    PointF[] points = new PointF[3];
+
+                    // 画出拆出的三角形
+                    for (int i = 0, j = 0; i < tris.Count; i += 3, j++) {
+                        if (j >= brushes.Length) j -= brushes.Length;
+                        int a = tris[i];
+                        int b = tris[i + 1];
+                        int c = tris[i + 2];
+                        points[0] = new PointF(polygon[a].x, polygon[a].y);
+                        points[1] = new PointF(polygon[b].x, polygon[b].y);
+                        points[2] = new PointF(polygon[c].x, polygon[c].y);
+                        g.FillPolygon(brushes[j], points);
+                    }
+                }
+            }
+            // 多边形的线
+            for (int i = 0, j = 1; i < polygon.Count; i++, j++) {
+                if (j >= polygon.Count) j -= polygon.Count;
+                g.DrawLine(Pens.Red, polygon[i].x, polygon[i].y, polygon[j].x, polygon[j].y);
+            }
+
+            // 多边形的顶点
+            for (int i = 0; i < polygon.Count; i++) {
+                Vec point = polygon[i];
+                Brush brush;
+                if (i == 0) brush = Brushes.Blue;
+                else brush = Brushes.Black;
+                g.FillEllipse(brush, point.x - 4, point.y - 4, 8, 8);
+            }
+        }
+    }
+
+}
